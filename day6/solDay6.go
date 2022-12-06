ab9d5ecd898c277be82dc085c06dc08b3a07b126
Shadow <66721908+Shadow21AR@users.noreply.github.com>
2022-12-06 16:06:33 +0530

    day6_better

--- day6/solDay6.go
+++ day6/solDay6.go
@@ -6,27 +6,22 @@ import (
 )
 
 func Day6() {
-	var res, res2 int
 	file, err := os.ReadFile("day6/inputDay6.txt")
 	if err != nil {
 		panic(err)
 	}
 	content := string(file)
-	for i := 1; i <= len(content)-3; i++ {
-		sop := content[i-1 : i+3]
-		if check(sop) {
-			res = i
-			break
-		}
-	}
-	for i := 1; i <= len(content)-13; i++ {
-		msg := content[i-1 : i+13]
+	fmt.Println("Answer 6_1: ", tracker(content, 3)+3, "\nAnswer 6_2: ", tracker(content, 13)+13)
+}
+func tracker(content string, n int) (out int) {
+	for i := 1; i <= len(content)-n; i++ {
+		msg := content[i-1 : i+n]
 		if check(msg) {
-			res2 = i
+			out = i
 			break
 		}
 	}
-	fmt.Println("Answer 6_1: ", res+3, "\nAnswer 6_2: ", res2+13)
+	return out
 }
 func check(sop string) bool {
 	var a [256]bool
